5757393b01edd217dc2e78108096a467b8b38db9
iva-a-a <ivanova.alena.a98@gmail.com>
2025-08-12 11:49:17 +0300

    update dependencies

--- Sources/di/containerProvider.swift
+++ Sources/di/containerProvider.swift
@@ -1,41 +1,50 @@
 //
-//  di/containerProvider.swift
+//  ContainerProvider.swift
 //  TicTacToe
 
 import Swinject
-import domain
-import datasource
-import web
+import Domain
+import Datasource
+import Web
+import Vapor
 
 public final class ContainerProvider {
     @MainActor public static let shared = ContainerProvider()
-     
-     public let container: Container
-     
-     private init() {
-         container = Container()
-         setupDependencies()
-     }
-     
-     private func setupDependencies() {
-
-         container.register(GameStore.self) { _ in
-             GameStore()
-         }.inObjectScope(.container)
-
-         container.register((any GameRepository).self) { resolver in
-             GameRepositoryImpl(store: resolver.resolve(GameStore.self)!)
-         }
-
-         container.register((any GameService).self) { _ in
-             GameServiceImpl()
-         }
-         
-         container.register(GameController.self) { resolver in
-             GameController(
+    
+    public let container: Container
+    
+    private init() {
+        container = Container()
+    }
+    
+    @MainActor public func setupDependencies(app: Application) {
+        
+        let db = app.db
+        
+        container.register((any GameRepository).self) { _ in
+            GameRepositoryDatabase(db: db)
+        }
+        container.register((any UserRepository).self) { _ in
+            UserRepositoryDatabase(db: db)
+        }
+        container.register((any GameService).self) { _ in
+            GameServiceImpl()
+        }
+        container.register((any UserService).self) { resolver in
+            UserServiceImpl(
+                userRepository: resolver.resolve((any UserRepository).self)!
+            )
+        }
+        container.register(GameController.self) { resolver in
+            GameController(
                 gameRepository: resolver.resolve((any GameRepository).self)!,
                 gameService: resolver.resolve((any GameService).self)!
-             )
-         }
-     }
+            )
+        }
+        container.register(UserController.self) { resolver in
+            UserController(
+                userService: resolver.resolve((any UserService).self)!
+            )
+        }
+    }
 }
