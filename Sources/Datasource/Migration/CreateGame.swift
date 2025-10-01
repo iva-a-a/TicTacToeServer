d8350ea14aed9ee5dcb6660b182c6da4a64c683d
iva-a-a <ivanova.alena.a98@gmail.com>
2025-10-01 19:01:56 +0300

    изменила бд для игр

--- Sources/Datasource/Migration/CreateGame.swift
+++ Sources/Datasource/Migration/CreateGame.swift
@@ -1,6 +1,7 @@
 //
 //  CreateGame.swift
 //  TicTacToe
+//
 
 import Foundation
 import Fluent
@@ -12,9 +13,16 @@ public struct CreateGame: AsyncMigration {
         try await database.schema("games")
             .id()
             .field("grid", .json, .required)
-            .field("state", .json, .required)
-            .field("players", .json, .required)
+            .field("state", .string, .required)
+            .field("playerX_id", .uuid)
+            .field("playerO_id", .uuid)
+            .field("playerX_login", .string)
+            .field("playerO_login", .string)
+            .field("current_turn_player_id", .uuid)
+            .field("winner_id", .uuid)
+            .field("is_draw", .bool, .required)
             .field("withAI", .bool, .required)
+            .field("created", .datetime, .required)
             .create()
     }
     
