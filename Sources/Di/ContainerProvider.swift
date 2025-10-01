a29e9a1ccd551f39766ac381dff4096311a84661
iva-a-a <ivanova.alena.a98@gmail.com>
2025-10-01 19:05:22 +0300

    добавила зависимости

--- Sources/Di/ContainerProvider.swift
+++ Sources/Di/ContainerProvider.swift
@@ -7,6 +7,7 @@ import Domain
 import Datasource
 import Web
 import Vapor
+import JWTKit
 
 public final class ContainerProvider {
     @MainActor public static let shared = ContainerProvider()
@@ -21,24 +22,41 @@ public final class ContainerProvider {
         
         let db = app.db
         
+        // JwtProvider
+        container.register((any JwtProvider).self) { _ in
+            JwtProviderImpl(secret: Environment.get("JWT_SECRET") ?? "secret")
+        }
+        
+        // Repositories
         container.register((any GameRepository).self) { _ in
             GameRepositoryDatabase(db: db)
         }
         container.register((any UserRepository).self) { _ in
             UserRepositoryDatabase(db: db)
         }
-        container.register((any GameService).self) { _ in
-            GameServiceImpl()
+        container.register((any GameStatsRepository).self) { _ in
+            GameStatsRepositoryDatabase(db: db)
+        }
+        
+        
+        // Services
+        container.register((any GameService).self) { resolver in
+            GameServiceImpl(gameRepository: resolver.resolve((any GameRepository).self)!)
         }
         container.register((any UserService).self) { resolver in
             UserServiceImpl(
-                userRepository: resolver.resolve((any UserRepository).self)!
+                userRepository: resolver.resolve((any UserRepository).self)!,
+                jwtProvider: resolver.resolve((any JwtProvider).self)!
             )
         }
+        container.register((any GameStatsService).self) { resolver in
+            GameStatsServiceImpl(gameStatsRepository: resolver.resolve((any GameStatsRepository).self)!)
+        }
+        
+        // Controllers
         container.register(GameController.self) { resolver in
             GameController(
-                gameRepository: resolver.resolve((any GameRepository).self)!,
-                gameService: resolver.resolve((any GameService).self)!
+                gameService: resolver.resolve((any GameService).self)!, gameStatsService: resolver.resolve((any GameStatsService).self)!
             )
         }
         container.register(UserController.self) { resolver in
@@ -48,3 +66,4 @@ public final class ContainerProvider {
         }
     }
 }
+
